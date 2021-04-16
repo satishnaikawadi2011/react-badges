f65d9b9bf48c79a078165555845d02ba07e3cebd
Satish Naikawadi <satishnaikawadi2001@gmail.com>
2021-04-16 11:00:39 +0530

    First deployment.

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,3 @@
 import * as React from 'react';
 import styles from './styles.module.css';
 export { default as Badge } from './components/Badge/Badge';
-interface Props {
-	text: string;
-}
-
-export const ExampleComponent = ({ text }: Props) => {
-	return <div className={styles.test}>Example Component Lassan: {text}</div>;
-};
